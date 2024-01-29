31f210bb3859c5253239a1f999ebc24ac98adf69
alainware <irving6258@gmail.com>
2024-01-29 00:29:36 -0600

    Update: Added Bootstrap

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 /* Vanilla JS Demo */
 const firebaseConfig = {
-/* Credentials */
+  /* Credentials */
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 /* Globals */
-let cpu_usage = firebase.database().ref().child('cpu_usage');
-let disk_usage = firebase.database().ref().child('disk_usage');
+const toastLiveExample = document.getElementById("liveToast");
+let toast_message = document.getElementById("toastMessage");
+let cpu_usage = firebase.database().ref().child("cpu_usage");
+let disk_usage = firebase.database().ref().child("disk_usage");
 /* Event Listeners */
 cpu_usage.on("value", (snapshot) => {
-    let cpu_usage_value = snapshot.val();
-    let cpu_object = document.getElementById('cpu');
-    cpu_object.innerText = cpu_usage_value;
-    console.log('CPU Usage changed: ' + cpu_usage_value); 
+  let cpu_usage_value = snapshot.val();
+  let cpu_object = document.getElementById("cpu");
+  cpu_object.innerText = cpu_usage_value;
+  console.log("CPU Usage changed: " + cpu_usage_value);
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toast_message.innerText = "CPU Usage changed: " + cpu_usage_value;
+  toastBootstrap.show();
 });
 disk_usage.on("value", (snapshot) => {
-    let disk_usage_value = snapshot.val();
-    let disk_object = document.getElementById('disk');
-    disk_object.innerText = disk_usage_value;
-    console.log('Disk Usage changed: ' + disk_usage_value); 
+  let disk_usage_value = snapshot.val();
+  let disk_object = document.getElementById("disk");
+  disk_object.innerText = disk_usage_value;
+  console.log("Disk Usage changed: " + disk_usage_value);
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  toast_message.innerText = "Disk Usage changed: " + disk_usage_value;
+  toastBootstrap.show();
 });
